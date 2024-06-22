7efa3fad6ab1f050e4eb00a4099b6c825c0d1fb7
Paul Osman <paul@eval.ca>
2024-06-22 16:13:33 -0400

    Add locking, more logging

--- db/db.go
+++ db/db.go
@@ -1,8 +1,10 @@
 package db
 
-type Database struct {
-	Events map[int32]*Event
+import "sync"
 
+type Database struct {
+	Events      map[int32]*Event
+	mutex       sync.Mutex
 	nextEventId int32
 }
 
@@ -14,22 +16,34 @@ func NewDatabase() *Database {
 }
 
 func (d *Database) AddEvent(event *Event) bool {
+	d.mutex.Lock()
 	if event.Id == 0 {
 		event.Id = d.nextEventId
 		d.nextEventId++
 	}
 	d.Events[event.Id] = event
+	d.mutex.Unlock()
 	return true
 }
 
 func (d *Database) GetEvents() []*Event {
+	d.mutex.Lock()
 	events := make([]*Event, 0)
 	for _, event := range d.Events {
 		events = append(events, event)
 	}
+	d.mutex.Unlock()
 	return events
 }
 
 func (d *Database) GetEvent(id int32) *Event {
-	return d.Events[id]
+	var event *Event
+	d.mutex.Lock()
+	event = d.Events[id]
+	d.mutex.Unlock()
+	return event
+}
+
+func (d *Database) DeleteEvent(id int32) {
+	d.Events[id] = nil
 }
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -20,6 +20,7 @@ func NewRouter() *mux.Router {
 	router.HandleFunc("/events", AddEventHandler(database)).Methods("POST")
 	router.HandleFunc("/events", GetEventsHandler(database))
 	router.HandleFunc("/events/{id:[0-9]+}", GetEventHandler(database))
+	router.HandleFunc("/events/{id:[0-9]+}", DeleteEventHandler(database)).Methods("DELETE")
 	router.HandleFunc("/events/{id:[0-9]+}/ticket", CreateTicketHandler(database)).Methods("POST")
 
 	return router
@@ -31,23 +32,36 @@ func StatusHandler() HandlerFunc {
 	}
 }
 
+func DeleteEventHandler(database *db.Database) HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id, err := strconv.Atoi(vars["id"])
+		if err != nil {
+			error(w, http.StatusBadRequest, r.Method, "/events/:id", "Invalid id provided: "+err.Error())
+			return
+		}
+		database.DeleteEvent(int32(id))
+		ok(w, r.Method, "/events/:id", "Deleted")
+	}
+}
+
 func AddEventHandler(database *db.Database) HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var event db.Event
 		decoder := json.NewDecoder(r.Body)
 		if err := decoder.Decode(&event); err != nil {
-			error(w, http.StatusBadRequest, "Error decoding payload: "+err.Error())
+			error(w, http.StatusBadRequest, r.Method, r.URL.Path, "Error decoding payload: "+err.Error())
 			return
 		}
 		defer r.Body.Close()
 		database.AddEvent(&event)
-		accepted(w, event)
+		accepted(w, r.Method, r.URL.Path, event)
 	}
 }
 
 func GetEventsHandler(database *db.Database) HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ok(w, database.GetEvents())
+		ok(w, r.Method, r.URL.Path, database.GetEvents())
 	}
 }
 
@@ -56,58 +70,59 @@ func GetEventHandler(database *db.Database) HandlerFunc {
 		vars := mux.Vars(r)
 		id, err := strconv.Atoi(vars["id"])
 		if err != nil {
-			error(w, http.StatusBadRequest, "Invalid id provided: "+err.Error())
+			error(w, http.StatusBadRequest, r.Method, "/events/:id", "Invalid id provided: "+err.Error())
 			return
 		}
 		event := database.GetEvent(int32(id))
 		if event == nil {
-			error(w, http.StatusNotFound, "no event found")
+			error(w, http.StatusNotFound, r.Method, "/events/:id", "no event found")
 			return
 		}
-		ok(w, event)
+		ok(w, r.Method, "/events/:id", event)
 	}
 }
 
 func CreateTicketHandler(database *db.Database) HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		path := "/events/:id/ticket"
 		vars := mux.Vars(r)
 		id, err := strconv.Atoi(vars["id"])
 		if err != nil {
-			error(w, http.StatusBadRequest, "Invalid id provided: "+err.Error())
+			error(w, http.StatusBadRequest, r.Method, path, "Invalid id provided: "+err.Error())
 			return
 		}
 		decoder := json.NewDecoder(r.Body)
 		var ticket db.Ticket
 		if err := decoder.Decode(&ticket); err != nil {
-			error(w, http.StatusBadRequest, "Error decoding payload: "+err.Error())
+			error(w, http.StatusBadRequest, r.Method, path, "Error decoding payload: "+err.Error())
 			return
 		}
 		event := database.GetEvent(int32(id))
 		if event.CapacityLeft() == 0 {
-			error(w, http.StatusForbidden, "Insufficient capacity for event")
+			error(w, http.StatusForbidden, r.Method, path, "Insufficient capacity for event")
 			return
 		}
 		ticket.EventId = int32(id)
 		event.Tickets = append(event.Tickets, ticket)
 		database.AddEvent(event)
 
-		ok(w, event)
+		ok(w, r.Method, path, event)
 	}
 }
 
-func error(w http.ResponseWriter, code int, message string) {
-	respond(w, code, map[string]string{"error": message})
+func error(w http.ResponseWriter, code int, method string, path string, message string) {
+	respond(w, code, method, path, map[string]string{"error": message})
 }
 
-func accepted(w http.ResponseWriter, body interface{}) {
-	respond(w, http.StatusAccepted, body)
+func accepted(w http.ResponseWriter, method string, path string, body interface{}) {
+	respond(w, http.StatusAccepted, method, path, body)
 }
 
-func ok(w http.ResponseWriter, body interface{}) {
-	respond(w, http.StatusOK, body)
+func ok(w http.ResponseWriter, method string, path string, body interface{}) {
+	respond(w, http.StatusOK, method, path, body)
 }
 
-func respond(w http.ResponseWriter, code int, body interface{}) {
+func respond(w http.ResponseWriter, code int, method string, path string, body interface{}) {
 	response, _ := json.Marshal(body)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
